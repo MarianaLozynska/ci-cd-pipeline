b9f3739ce738437208715b4e19518042753c8141
Mariana Lozynska <mariana.lozynska@imgarena.com>
2023-02-24 13:57:27 +0000

    added version

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={pipeline} className="App-logo" alt="logo" />
         {/* <p>CI/CD Pipeline</p> */}
-        <p>Release:</p>
+        <p>Release: v0.0.1</p>
         <a
           className="CI/CD-link"
           href="https://en.wikipedia.org/wiki/CI/CD"
